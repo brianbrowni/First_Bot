f6aa34589bd9d2be1f022d87e0575dd136a00065
Brian Browni <brian.cb237@gmail.com>
2022-03-04 22:37:03 +0100

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-//OTQ3OTM1NTI5MjU5OTE3NDEy.Yh0fwQ.iRB-Pf1ThyHoD7VxZLSryeS0Pk4
-
 const Discord = require("discord.js");
 const { Client, Intents} = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const keys = require('./config.json');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -14,4 +13,4 @@ client.on("message", msg => {
     }
 })
 
-client.login("OTQ3OTM1NTI5MjU5OTE3NDEy.Yh0fwQ.KmwzkIiDii_Rq7dRDf4O2jIkNrw");
+client.login(keys.TOKEN);
